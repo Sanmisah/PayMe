a7c6948b14f718c29aa752b5185ee62ccac12dcd
Vinod Sarode <vs10201020@gmail.com>
2023-05-23 19:38:03 +0530

    first commit

--- database/migrations/2023_05_23_175854_create_loans_table.php
+++ database/migrations/2023_05_23_175854_create_loans_table.php
@@ -16,17 +16,17 @@ return new class extends Migration
         Schema::create('loans', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->integer('mobile_no')->nullable();
-            $table->integer('alternative_no')->nullable();
+            $table->string('mobile_no')->nullable();
+            $table->string('alternative_no')->nullable();
             $table->string('email')->unique()->nullable();
             $table->string('address')->nullable();
             $table->foreignId('area_id')->nullable();
             $table->string('contact_person')->nullable();
-            $table->integer('contact_person_no')->nullable();
+            $table->string('contact_person_no')->nullable();
             $table->string('contact_person_email')->nullable();
             $table->decimal('amount', 10, 2)->nullable();
             $table->decimal('final_amount', 10, 2)->nullable();
-            $table->integer('interest_rate')->nullable();
+            $table->string('interest_rate')->nullable();
             $table->string('amount_in_words')->nullable();
             $table->foreignId('agent_id')->nullable();
             $table->timestamps();
